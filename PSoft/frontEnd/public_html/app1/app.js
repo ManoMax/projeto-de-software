465a3ed088421acac67d77551698354a3e252401
ManoMax <gabrielmaxcontato@gmail.com>
2019-09-05 15:40:49 -0300

    atualizando estudos Web

---
+++ PSoft/frontEnd/public_html/app1/app.js
@@ -0,0 +1,24 @@
+console.log('app rodando!');
+
+let disciplinas = [];
+let $disciplinas = document.querySelector("div");
+function fetch_disciplinas() {
+    let URL = 'https://lab01-projsw-ufcg.herokuapp.com/api/disciplinas';
+    fetch(URL)
+    .then(response => response.json())
+    .then(dados => {
+        console.log(dados);
+        handler(dados);
+    })
+}
+
+function handler(dados) {
+    disciplinas = dados;
+    $disciplinas.innerHTML = '';
+    disciplinas.forEach((element, indice)  => {
+        let $p = document.createElement("p");
+        $disciplinas.appendChild($p);
+        $p.innerText = "Disciplina: " + disciplinas[indice].nome + " - Nota: " + disciplinas[indice].nota;
+    });
+}
+fetch_disciplinas();
