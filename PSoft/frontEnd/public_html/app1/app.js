add9eb4be363915e1ecddab79b7bba1867eb1b6e
ManoMax <gabrielmaxcontato@gmail.com>
2019-09-10 11:39:53 -0300

    POST atualizando DOM em tempo real

--- PSoft/frontEnd/public_html/app1/app.js
+++ PSoft/frontEnd/public_html/app1/app.js
@@ -5,6 +5,7 @@ let $disciplinas = document.querySelector("div");
 let URL = 'https://lab01-projsw-ufcg.herokuapp.com/api';
 
 let $button = document.getElementById("botao");
+let $mensagem = document.getElementById("mensagem");
 
 function fetch_disciplinas() {
     fetch(URL + '/disciplinas')
@@ -34,13 +35,16 @@ function save() {
         'body': `{"nome": "${nome}", "nota": ${nota}}`,
         'headers': {'Content-Type': 'application/json'}
     })
-    .then(r => {
-        r.json();
-        console.log(r);
-    })
+    .then(r => r.json())
     .then(d => {
         console.log(d);
+        $mensagem.innerText = 'Disciplina salva';
+        setTimeout(_ => {
+            $mensagem.innerText = '';
+        }, 2000);
+        fetch_disciplinas();
     })
+    $mensagem.innerText = 'Salvando...';
 }
 
 fetch_disciplinas();
