8e89cc97f7dc68c6609b3af5e02cbc6f7085c82a
ManoMax <gabrielmaxcontato@gmail.com>
2019-09-25 11:17:40 -0300

    Leitura de aquivos JSON em lista

--- PSoft/backEnd_Lab2/psoft/src/main/java/lab2/psoft/services/DisciplinaServices.java
+++ PSoft/backEnd_Lab2/psoft/src/main/java/lab2/psoft/services/DisciplinaServices.java
@@ -1,23 +1,39 @@
 package lab2.psoft.services;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Optional;
-
-// import javax.annotation.PostConstruct;
+import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import lab2.psoft.daos.DisciplinasRepository;
 import lab2.psoft.models.Disciplina;
+import lab2.psoft.daos.DisciplinasRepository;
 
 @Service
 public class DisciplinaServices {
 
-    // @Autowired
+    @Autowired
 	private DisciplinasRepository<Disciplina, Long> disciplinasDAO;
-	
-	//obrigatorio ter esse construtor, caso contrario chama um construtor
-    //default e o DAO fica null
+
+    @PostConstruct
+    public void initDisciplinas() {
+        ObjectMapper mapper = new ObjectMapper();
+        TypeReference<List<Disciplina>> typeReference = new TypeReference<List<Disciplina>>(){};
+        InputStream inputStream = ObjectMapper.class.getResourceAsStream("/json/disciplinas.json");
+        try {
+            List<Disciplina> disciplinas = mapper.readValue(inputStream, typeReference);
+            this.disciplinasDAO.saveAll(disciplinas);
+            System.out.println("Disciplina salva no BD");
+        } catch (IOException e) {
+            System.out.println("Não foi possível salvar as Disciplinas: " + e.getMessage());
+        }
+    }
+
 	public DisciplinaServices(DisciplinasRepository<Disciplina, Long> disciplinasDAO) {
 		super();
 		this.disciplinasDAO = disciplinasDAO;
