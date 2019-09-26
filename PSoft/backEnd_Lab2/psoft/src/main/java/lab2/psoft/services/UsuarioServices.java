0400e324e8f65dcc3cd4268540468e9a3fa8c289
ManoMax <gabrielmaxcontato@gmail.com>
2019-09-26 14:45:12 -0300

    Identificacao de Service

--- PSoft/backEnd_Lab2/psoft/src/main/java/lab2/psoft/services/UsuarioServices.java
+++ PSoft/backEnd_Lab2/psoft/src/main/java/lab2/psoft/services/UsuarioServices.java
@@ -3,7 +3,9 @@ package lab2.psoft.services;
 import lab2.psoft.daos.UsuariosRepository;
 import lab2.psoft.models.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UsuarioServices {
 
     @Autowired
